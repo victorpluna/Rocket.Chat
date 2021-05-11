37d42d59a6dc4376946566fba38b3691be1db61c
Victor Luna <victorluna22@gmail.com>
2021-05-10 21:33:31 -0300

    Add new fields into metadata form

--- client/views/room/contextualBar/StateFiles/StateFiles.js
+++ client/views/room/contextualBar/StateFiles/StateFiles.js
@@ -1,8 +1,6 @@
 import React, { useMemo, useCallback } from 'react';
-import { useUniqueId, useMutableCallback } from '@rocket.chat/fuselage-hooks';
-import { Box, Button, Field, Select, TextInput, TextAreaInput, ToggleSwitch, InputBox, Chip, MultiSelectFiltered, UrlInput } from '@rocket.chat/fuselage';
+import { Box, Button, Field, Select, TextInput, TextAreaInput, MultiSelectFiltered } from '@rocket.chat/fuselage';
 
-import UserAutoCompleteMultiple from '../../../../../ee/client/audit/UserAutoCompleteMultiple';
 import VerticalBar from '../../../../components/VerticalBar';
 import Page from '../../../../components/Page';
 import { useTranslation } from '../../../../contexts/TranslationContext';
@@ -10,118 +8,140 @@ import { useTabBarClose } from '../../providers/ToolboxProvider';
 import { useForm } from '../../../../hooks/useForm';
 
 const initialValues = {
-	title: '',
+	name: '',
 	department: null,
 	disposalRuleAuthority: null,
 	authorityNumber: null,
-	activityNumber: null,
-	type: null,
-	purpose: null,
-	publicAccess: false,
-	publicAccessType: null,
+	disposalClassId: null,
 	description: '',
-	files: [],
-	users: [],
-	tags: [],
-	links: [],
+	jurisdiction: null,
+	securityClassification: null,
+	caveatCategory: null,
+	caveat: '',
+	permissionType: null,
+	permission: null,
+	rightsType: null,
+	rightsStatus: null,
+	keywords: [],
+	location: null,
+	documentForm: null,
+	purpose: null,
+	limitingMarkers: null,
 };
 
 const StateFiles = () => {
 	const t = useTranslation();
 	const onClickClose = useTabBarClose();
-	const fileSourceInputId = useUniqueId();
 
 	const { values, handlers } = useForm(initialValues);
 	const {
-		title,
+		name,
 		department,
 		disposalRuleAuthority,
 		authorityNumber,
-		activityNumber,
-		type,
-		purpose,
-		publicAccess,
-		publicAccessType,
+		disposalClassId,
 		description,
-		files,
-		users,
-		links,
+		jurisdiction,
+		securityClassification,
+		caveatCategory,
+		caveat,
+		permissionType,
+		permission,
+		rightsType,
+		rightsStatus,
+		// keywords,
+		location,
+		documentForm,
+		purpose,
+		limitingMarkers,
 	} = values;
 
 	const {
-		handleTitle,
+		handleName,
 		handleDepartment,
 		handleDisposalRuleAuthority,
 		handleAuthorityNumber,
-		handleActivityNumber,
-		handleType,
-		handlePurpose,
-		handlePublicAccess,
-		handlePublicAccessType,
+		handleDisposalClassId,
 		handleDescription,
-		handleFiles,
-		handleUsers,
-		handleTags,
-		handleLinks,
+		handleJurisdiction,
+		handleSecurityClassification,
+		handleCaveatCategory,
+		handleCaveat,
+		handlePermissionType,
+		handlePermission,
+		handleRightsType,
+		handleRightsStatus,
+		handleKeywords,
+		handleLocation,
+		handleDocumentForm,
+		handlePurpose,
+		handleLimitingMarkers,
 	} = handlers;
 
 	const departmentOptions = useMemo(() => [
 		['department-premier-cabinet', 'Department of Premier and Cabinet'],
-	], [t]);
+	], []);
 
 	const disposalRuleAuthorityOptions = useMemo(() => [
 		['gda13', 'GDA13'],
-	], [t]);
+	], []);
 
 	const authorityNumberOptions = useMemo(() => [
 		['fa254', 'FA254'],
-	], [t]);
+	], []);
 
-	const activityNumberOptions = useMemo(() => [
+	const disposalClassIdOptions = useMemo(() => [
 		['1.2.4', '1.2.4'],
-	], [t]);
+	], []);
 
-	const typeOptions = useMemo(() => [
-		['meeting-formal', 'Meeting Formal'],
-	], [t]);
+	const jurisdictionOptions = useMemo(() => [
+		['au', 'AU'],
+	], []);
+
+	const securityClassificationOptions = useMemo(() => [
+		['confidential', 'Confidential'],
+	], []);
+
+	const caveatCategoryOptions = useMemo(() => [
+		['agao', 'AGAO'],
+	], []);
+
+	const permissionTypeOptions = useMemo(() => [
+		['record-keeping', 'Record Keeping'],
+	], []);
+
+	const permissionOptions = useMemo(() => [
+		['official-sensitive-security', 'Official - sensitive - Security clearance'],
+	], []);
+
+	const rightsTypeOptions = useMemo(() => [
+		['intellectual-property', 'Intellectual Property'],
+	], []);
+
+	const rightsStatusOptions = useMemo(() => [
+		['released-under-foi', 'Mey be released under FOI'],
+	], []);
+
+	const locationOptions = useMemo(() => [
+		['canberra-AU', 'Canberra, Australia'],
+	], []);
+
+	const documentFormOptions = useMemo(() => [
+		['meeting-minutes', 'Meeting Minutes'],
+	], []);
 
 	const purposeOptions = useMemo(() => [
 		['approve-action', 'Approve Action'],
-	], [t]);
+	], []);
 
-	const publicAccessTypeOptions = useMemo(() => [
-		['delayed-release', 'Delayed Release'],
-	], [t]);
+	const limitingMarkersOptions = useMemo(() => [
+		['sensitive-cabinet', 'Sensitive: Cabinet'],
+	], []);
 
-	const tagOptions = useMemo(() => [
+	const keywordsOptions = useMemo(() => [
 		['teste1', 'Teste 1'],
 		['teste2', 'Teste 2'],
-	], [t]);
-
-	const handleImportFileChange = async (event) => {
-		event = event.originalEvent || event;
-
-		let { files } = event.target;
-		if (!files || (files.length === 0)) {
-			files = (event.dataTransfer != null ? event.dataTransfer.files : undefined) || [];
-		}
-
-		handleFiles(Array.from(files));
-	};
-
-	const handleFileUploadChipClick = (file) => () => {
-		handleFiles((files) => files.filter((_file) => _file !== file));
-	};
-
-	const onChangeUsers = useMutableCallback((value, action) => {
-		if (!action) {
-			if (users.includes(value)) {
-				return;
-			}
-			return handleUsers([...users, value]);
-		}
-		handleUsers(users.filter((current) => current !== value));
-	});
+	], []);
 
 	const onUpload = useCallback(() => {
 		console.log('onUpload', values);
@@ -130,20 +150,19 @@ const StateFiles = () => {
 	return (
 		<>
 			<VerticalBar.Header>
-				<VerticalBar.Text>{t('create-state-record')}</VerticalBar.Text>
-				<Button onClick={onUpload} small primary>{t('Save')}</Button>
+				<VerticalBar.Text>{t('Chat_Metadata')}</VerticalBar.Text>
 				{ onClickClose && <VerticalBar.Close onClick={onClickClose} /> }
 			</VerticalBar.Header>
 			<Page.ScrollableContent p={null}>
 				<VerticalBar.Section borderBlockEndWidth='x2' borderBlockColor='neutral-200'>
 					<Field>
-						<Field.Label>{t('Title')}</Field.Label>
+						<Field.Label>{t('Name')}</Field.Label>
 						<Field.Row>
 							<TextInput
 								error={undefined}
-								placeholder={t('Title')}
-								value={title}
-								onChange={handleTitle}
+								placeholder={t('Name')}
+								value={name}
+								onChange={handleName}
 							/>
 						</Field.Row>
 					</Field>
@@ -167,37 +186,12 @@ const StateFiles = () => {
 							</Field.Row>
 						</Field>
 						<Field width='160px'>
-							<Field.Label>{t('Activity_Number')}</Field.Label>
+							<Field.Label>{t('Disposal_Class_Id')}</Field.Label>
 							<Field.Row>
-								<Select value={activityNumber} onChange={handleActivityNumber} options={activityNumberOptions} placeholder={t('Activity')} />
+								<Select value={disposalClassId} onChange={handleDisposalClassId} options={disposalClassIdOptions} placeholder={t('Class_Id')} />
 							</Field.Row>
 						</Field>
 					</Box>
-				</VerticalBar.Section>
-				<VerticalBar.Section borderBlockEndWidth='x2' borderBlockColor='neutral-200'>
-					<Box display='flex' flexDirection='row' justifyContent='space-between'>
-						<Field width='160px'>
-							<Field.Label>{t('Type')}</Field.Label>
-							<Field.Row>
-								<Select value={type} onChange={handleType} options={typeOptions} placeholder={t('Type')} />
-							</Field.Row>
-						</Field>
-						<Field width='160px'>
-							<Field.Label>{t('Purpose')}</Field.Label>
-							<Field.Row>
-								<Select value={purpose} onChange={handlePurpose} options={purposeOptions} placeholder={t('Purpose')} />
-							</Field.Row>
-						</Field>
-					</Box>
-					<Field>
-						<Field.Row>
-							<Field.Label>{t('Public_Access')}</Field.Label>
-							<ToggleSwitch checked={publicAccess} onChange={handlePublicAccess} />
-						</Field.Row>
-						<Field.Row>
-							<Select value={publicAccessType} onChange={handlePublicAccessType} options={publicAccessTypeOptions} placeholder={t('Public_Access')} />
-						</Field.Row>
-					</Field>
 					<Field>
 						<Field.Label>{t('Description')}</Field.Label>
 						<Field.Row>
@@ -209,33 +203,92 @@ const StateFiles = () => {
 							/>
 						</Field.Row>
 					</Field>
+
 					<Field>
-						<Field.Label>{t('Attach_Documents')}</Field.Label>
-						<InputBox type='file' id={fileSourceInputId} onChange={handleImportFileChange} />
-						{files?.length > 0 && (
-							<Field.Row>
-								{files.map((file, i) => <Chip key={i} onClick={handleFileUploadChipClick(file)}>{file.name}</Chip>)}
-							</Field.Row>
-						)}
-					</Field>
-					<Field>
-						<Field.Label>{t('Who_Was_Envolved')}</Field.Label>
-						<UserAutoCompleteMultiple value={users} onChange={onChangeUsers} placeholder={t('Please_enter_usernames')} />
-					</Field>
-				</VerticalBar.Section>
-				<VerticalBar.Section>
-					<Field>
-						<Field.Label>{t('Tags')}</Field.Label>
-						<MultiSelectFiltered options={tagOptions} onChange={handleTags} />
-					</Field>
-					<Field>
-						<Field.Label>{t('Links')}</Field.Label>
+						<Field.Label>{t('Jurisdiction')}</Field.Label>
 						<Field.Row>
-							<UrlInput
-								value={links}
-								onChange={handleLinks}
+							<Select value={jurisdiction} onChange={handleJurisdiction} options={jurisdictionOptions} placeholder={t('Jurisdiction')} />
+						</Field.Row>
+					</Field>
+					<Box display='flex' flexDirection='row' justifyContent='space-between'>
+						<Field width='160px'>
+							<Field.Label>{t('Security_Classification')}</Field.Label>
+							<Field.Row>
+								<Select value={securityClassification} onChange={handleSecurityClassification} options={securityClassificationOptions} placeholder={t('Classification')} />
+							</Field.Row>
+						</Field>
+						<Field width='160px'>
+							<Field.Label>{t('Caveat_Category')}</Field.Label>
+							<Field.Row>
+								<Select value={caveatCategory} onChange={handleCaveatCategory} options={caveatCategoryOptions} placeholder={t('Category')} />
+							</Field.Row>
+						</Field>
+					</Box>
+					<Field>
+						<Field.Label>{t('Caveat')}</Field.Label>
+						<Field.Row>
+							<TextInput
+								error={undefined}
+								placeholder={t('Caveat')}
+								value={caveat}
+								onChange={handleCaveat}
 							/>
 						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Permission_Type')}</Field.Label>
+						<Field.Row>
+							<Select value={permissionType} onChange={handlePermissionType} options={permissionTypeOptions} placeholder={t('Permission_Type')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Permission')}</Field.Label>
+						<Field.Row>
+							<Select value={permission} onChange={handlePermission} options={permissionOptions} placeholder={t('Permission')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Rights_Type')}</Field.Label>
+						<Field.Row>
+							<Select value={rightsType} onChange={handleRightsType} options={rightsTypeOptions} placeholder={t('Rights_Type')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Rights_Status_Scheme')}</Field.Label>
+						<Field.Row>
+							<Select value={rightsStatus} onChange={handleRightsStatus} options={rightsStatusOptions} placeholder={t('Rights_Status_Scheme')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Keywords')}</Field.Label>
+						<MultiSelectFiltered options={keywordsOptions} onChange={handleKeywords} />
+					</Field>
+					<Field>
+						<Field.Label>{t('Location')}</Field.Label>
+						<Field.Row>
+							<Select value={location} onChange={handleLocation} options={locationOptions} placeholder={t('Location')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Document_Form')}</Field.Label>
+						<Field.Row>
+							<Select value={documentForm} onChange={handleDocumentForm} options={documentFormOptions} placeholder={t('Document_Form')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Purpose')}</Field.Label>
+						<Field.Row>
+							<Select value={purpose} onChange={handlePurpose} options={purposeOptions} placeholder={t('Purpose')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Dissemination_Limiting_Markers')}</Field.Label>
+						<Field.Row>
+							<Select value={limitingMarkers} onChange={handleLimitingMarkers} options={limitingMarkersOptions} placeholder={t('Dissemination_Limiting_Markers')} />
+						</Field.Row>
+					</Field>
+					<Field>
+						<Button onClick={onUpload} primary>{t('Save')}</Button>
 					</Field>
 				</VerticalBar.Section>
 			</Page.ScrollableContent>
